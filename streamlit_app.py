87ecbfcc764e74812b39e89696303e9648886d54
Shirisha-Arukala <121345058+Shirisha-Arukala@users.noreply.github.com>
2022-12-24 19:23:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale Spinach & Rocket Smoothie')
