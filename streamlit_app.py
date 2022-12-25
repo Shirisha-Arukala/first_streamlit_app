194773c739ee02076e098affd037de8f90dcd4c0
Shirisha-Arukala <121345058+Shirisha-Arukala@users.noreply.github.com>
2022-12-25 20:45:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,5 @@ streamlit.dataframe(my_fruit_list)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
